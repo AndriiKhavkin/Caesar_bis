acec4c0d0bd3f0fb19a06d213e1157289949ab5d
AndriiKhavkin <tapusti7@gmail.com>
2023-02-26 18:49:41 +0200

    Commit

---
+++ Caesar_bis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;  
+
+namespace Caesar_bis
+{
+    internal class Program
+    {
+        
+        public static void Main()
+        { 
+        
+            Console.WriteLine("Type a string to encrypt:");  
+            string UserString = Console.ReadLine();  
+  
+            Console.WriteLine("Enter your Key:");  
+            int key = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Encrypted Data"); 
+            string cipherText = Logic.Encipher(UserString, key);
+            Console.WriteLine(cipherText);  
+            
+            Console.WriteLine("Decrypted Data:");
+            string decryptedText = Logic.Decipher(cipherText, key);  
+            Console.WriteLine(decryptedText); 
+            
+            Console.ReadKey();  
+        }
+    }
+}
